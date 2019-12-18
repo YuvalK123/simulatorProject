27ce69136ec47f2caa35c0fe2a43476ad29afdc5
yuvalkasner <yuvaldubi1@gmail.com>
2019-12-18 13:04:36 +0200

    start project 18.12

---
+++ cmake-build-debug/CMakeFiles/simulatorProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulatorProject.dir/main.cpp.o"
+  "simulatorProject"
+  "simulatorProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulatorProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
